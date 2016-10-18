2b8decddeb7e69fe202cdfd9967dcdbe45608d7d
Margaret <margaret@apples-MacBook-Air.local>
2016-10-17 21:20:03 -0700

    updated game logic per Unit 9 instuctions

--- starter-code/main.js
+++ starter-code/main.js
@@ -5,3 +5,16 @@ var cardTwo = "queen";
 var cardThree = "king";
 var cardFour = "king";
 /* Added variables and values as requested in Unit 8 Assignment */
+
+var firstCard = cardTwo;
+var secondCard = cardFour;
+
+if (((firstCard == "king") && (secondCard == "queen")) || ((firstCard == "queen") && (secondCard == "king"))) {
+	alert("Sorry, try again.");
+} else if ((firstCard == secondCard) && (firstCard == "king")) {
+	alert("You found a match!");
+} else if ((firstCard == secondCard) && (firstCard == "queen")) {
+	alert("You found a match!");
+} else {
+	alert("Sorry, try again.");
+}
