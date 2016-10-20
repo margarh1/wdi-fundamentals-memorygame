234c154b68a8e6c9f257e632cba3998d58045de6
Margaret <margaret@apples-MacBook-Air.local>
2016-10-20 00:30:41 -0700

    streamlined code per Unit 11 instructions, still one display bug

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,36 +1,43 @@
 console.log("JS file is connected to HTML! Woo!")
 
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
-/* Added variables and values as requested in Unit 8 Assignment */
-
-var firstCard = cardTwo;
-var secondCard = cardFour;
-
-/* if (((firstCard == "king") && (secondCard == "queen")) || ((firstCard == "queen") && (secondCard == "king"))) {
-	alert("Sorry, try again.");
-} else if ((firstCard == secondCard) && (firstCard == "king")) {
-	alert("You found a match!");
-} else if ((firstCard == secondCard) && (firstCard == "queen")) {
-	alert("You found a match!");
-} else {
-	alert("Sorry, try again.");
-}*/
-/* Added if...else statement per Unit 9 Assignment instructions */
-
 var gameBoard = document.getElementById("game-board");
 
-var createCards = function(number) {
-	for (number; number > 0; number--) {
+var cards = ['queen', 'queen', 'king', 'king'];
+var cardsInPlay = [];
+
+var isMatch = function(array) {
+	if (array[0] == array[1]) {
+		alert("You found a match!");
+	} else {
+		alert("Sorry, try again.");
+	}
+	gameBoard.innerHTML = '';
+	createBoard(cards);
+}
+
+var isTwoCards = function() {
+	cardsInPlay.push(this.getAttribute('data-card'));
+	if (this.getAttribute('data-card') == 'queen') {
+		this.innerHTML = '<img src="queen.png" alt="Queen of Diamonds" />';
+	} else if (this.getAttribute('data-card') == 'king') {
+		this.innerHTML = '<img src="king.png" alt="King of Spades" />';
+	}
+	if (cardsInPlay.length === 2) {
+		isMatch(cardsInPlay);
+		cardsInPlay = [];
+	}
+}
+
+var createBoard = function(cards) {
+	for (var idx = 0; idx < cards.length; idx++) {
 		var newCard = document.createElement("div");
 		newCard.setAttribute("class", "card");
+		newCard.setAttribute("data-card", cards[idx]);
+		newCard.addEventListener("click", isTwoCards);
 		gameBoard.appendChild(newCard);
 	}
 }
 
-createCards(4);
-// Commented out div elements and created function to make div elements per Unit 10 Assignment instructions.
-
-
+createBoard(cards);
+// Adjusted and streamlined per Unit 11 Assignment instructions
+// Still has a display bug to try to fix
